d2881d91cecc4a75e87c828bc501554da3b17f34
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-09-06 20:39:35 +0100

    update on files

---
+++ 100-days_of_Java/day-1/Operations.java
@@ -0,0 +1,27 @@
+public class Operations {
+	public static void main(String[] args) {
+		// perform addition
+		int result = 2 + 4;
+		System.out.println("Addition of 2 and 4=" + result);
+		
+		// perform substraction
+		result = 4 - 2;
+		System.out.println("Substraction of 2 from 4=" + result);
+		
+		// perform multiplication
+		result = 2 * 2;
+		System.out.println("mulitplication of 2 by 2=" + result);
+		
+		// perform bitwise or
+		byte b_result = 2 | 3;
+		System.out.println("Bitwise 'or' of 2 and 3=" + b_result);
+		
+		// perform bitwise and
+		b_result = 2 & 3;
+		System.out.println("Bitwise 'and' of 2 and 3=" + b_result);
+		
+		// perform bitwise XOR
+		b_result = 2 ^ 3;
+		System.out.println("Bitwise 'XOR' on 2 and 3=" + b_result);
+	}
+}
