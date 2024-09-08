1a521f8def0221fd7e963a347a01de0b520bb4d0
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-09-08 22:58:22 +0100

    class constructor and static methods

--- 100-days_of_Java/day-3/Book.java
+++ 100-days_of_Java/day-3/Book.java
@@ -3,26 +3,38 @@
 
 
 public class Book {
-	static String title;
-	static String author;
-	static int year;
+	private String title;
+	private String author;
+	private int year;
+	private static int nBooks = 0; // number of books created
 
-	static void showDetails() {
+	public Book(String title, String author, int year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		nBooks += 1; 
+	}
+
+	public void showDetails() {
 		System.out.printf("Title: %s - Author: %s - Year: %d\n",
 				title, author, year);
 	}
 
+	public static int getTotalBooks() {
+		return nBooks;
+	}
+
 	public static void main(String[] args) {
-		Book book1 = new Book();
-		book1.title = "Essential Mathematics";
-		book1.author = "Akachi Anabanti";
-		book1.year = 2025;
+		Book book1 = new Book("Essential Mathematics",
+				"Akachi Anabanti", 2025);
 		book1.showDetails();
 
-		Book book2 = new Book();
-		book2.title = "Essential Economics";
-		book2.author = "Ogochukwu Anabanti";
-		book2.year = 2028;
-		book2. showDetails();
+		System.out.printf("Current number of books = %d\n", Book.getTotalBooks());
+
+		Book book2 = new Book("Essential Economics",
+				"Ogochukwu Anabanti", 2028);
+		book2.showDetails();
+		
+		System.out.printf("Current number of books = %d\n", Book.getTotalBooks());
 	}
 }
