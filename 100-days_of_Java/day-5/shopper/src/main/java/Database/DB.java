be3e1e3224e93ba5b5340d14c199b1834c0e1de2
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-09-21 07:59:56 +0100

    database class

---
+++ 100-days_of_Java/day-5/shopper/src/main/java/Database/DB.java
@@ -0,0 +1,37 @@
+package shopper.src.main.java.Database;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import shopper.src.main.java.utils.JsonWriter;
+
+public class DB {
+    private static void saveObjToFile(Object ObjectClass) {
+        String fileName = ObjectClass.getClass().getName();
+        File objFile = new File(fileName);
+        if (!objFile.exists()) {
+            try {
+                objFile.createNewFile();
+                JsonWriter.writeObjectToFile(ObjectClass, objFile.getAbsolutePath());
+            } catch (IOException | IllegalAccessException e) {
+                System.out.println("Failed to create " + fileName);
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                JsonWriter.writeObjectToFile(ObjectClass, fileName);
+            } catch (IOException | IllegalAccessException e) {
+                System.out.println("Something went wrong, couldn't write to file " + fileName);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static <K, V> void save(Map<K, V> store) {
+        for(V obj: store.values()) {
+            saveObjToFile(obj);
+        }
+        System.out.println("Changes Saved!");
+    }
+}
